31f721da162a0e3f17f33de9bc8879d04cca71f9
Derandi Hermanda <derandi.hermanda@hotmail.com>
2020-08-18 10:04:50 +0700

    fix connection string

--- db/db.go
+++ db/db.go
@@ -10,7 +10,7 @@ import (
 func Connect() *sql.DB {
 	var err error
 	// db, err := sql.Open("mysql", "root:@/kasumi_development")
-	db, err := sql.Open("mysql", "root:P%40ssw0rdKasum1@/kasumi_development")
+	db, err := sql.Open("mysql", "root:P%40ssw0rdKasum1@tcp(localhost:8082)/kasumi_development")
 
 	if err != nil {
 		log.Fatal(err)
