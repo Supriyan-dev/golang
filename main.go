e4b7ddf8b2fdd571ffbb18f5f2d149e70404b7af
maskokohasan <harisandy21@gmail.com>
2020-09-02 14:42:05 +0700

    test 3 error in docker

--- main.go
+++ main.go
@@ -6,10 +6,7 @@ import (
 	controllerPermissionToDrive "./controller/list_input_information"
 	"fmt"
 	"github.com/rs/cors"
-	"log"
 	"net/http"
-	"os"
-
 	// controllerDataMaster "github.com/jeffri/golang-test/GO_DX_SERVICES/controller/data_master_controller"
 	// controllerPermissionToDrive "github.com/jeffri/golang-test/GO_DX_SERVICES/controller/list_input_information"
 
@@ -155,12 +152,12 @@ func main() {
 	// end Commuting Transportation Application
 
 	fmt.Println("Connected to port 9000")
-	file, errfile := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if errfile != nil {
-		log.Fatal(errfile)
-	}
-
-	log.SetOutput(file)
+	//file, errfile := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	//if errfile != nil {
+	//	log.Fatal(errfile)
+	//}
+	//
+	//log.SetOutput(file)
 
 	handler := cors.AllowAll().Handler(router)
 	http.ListenAndServe(":9000", handler)
