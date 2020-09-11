d7a7a68475fc3274fc9b09d455e89140a56a7436
maskokohasan <harisandy21@gmail.com>
2020-09-11 17:29:02 +0700

    handle big request

--- db/db.go
+++ db/db.go
@@ -27,8 +27,8 @@ func Connect() *sql.DB {
 
 func KillSleepConnection()  {
 
-	db, err := sql.Open("mysql", "root:@/kasumi_development")
-	//db, err := sql.Open("mysql", "godx1:G0LangDX_1@tcp(mysql_lara:3306)/kasumi_development")
+	//db, err := sql.Open("mysql", "root:@/kasumi_development")
+	db, err := sql.Open("mysql", "godx1:G0LangDX_1@tcp(mysql_lara:3306)/kasumi_development")
 
 	if err != nil {
 		log.Fatal(err)
